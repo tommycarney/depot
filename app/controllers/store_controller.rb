89bada69b1f1211bbc79ad73919da8d0207114bd
Thomas Carney <tommycarney@gmail.com>
2017-08-07 08:43:53 +0200

    creates cart + line item models; adds cart functionality

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
+  before_action :increment_visits, only: [:index]
+
   def index
     @products = Product.order(:title)
+    @visits = visits
+  end
+
+  private
+
+  def increment_visits
+    session[:visits] ||= 1
+    session[:visits] += 1
+  end
+
+  def visits
+    session[:visits]
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -14,6 +14,8 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select ".list_description", minimum: 3
   end
 
   test "should get new" do
@@ -45,13 +47,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to product_url(@product)
   end
 
-  # ...
+  test "can not delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+    assert_redirected_to products_url
+  end
 
   test "should destroy product" do
     assert_difference('Product.count', -1) do
       delete product_url(@product)
     end
-
     assert_redirected_to products_url
   end
 end
